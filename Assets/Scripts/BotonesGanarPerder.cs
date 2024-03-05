575decb11420a982977b6ef87e43ee22a5609b29
informaticaesne <informatica@esne.es>
2024-03-05 15:19:47 +0100

    Añadido botones al menú de ganar y perder
    Ahora los botones existen y funcionan

---
+++ Assets/Scripts/BotonesGanarPerder.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonesGanarPerder : MonoBehaviour
+{
+    public void VolverMenuPrincipal()
+    {
+        OpcionesNivelesManager.instanciaOpcionesNivel.ResetOpciones();
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+
+    public void ReiniciarNivel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
